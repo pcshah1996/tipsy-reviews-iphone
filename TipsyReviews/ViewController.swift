cea703ef515d4122b993e6ee49cc6dade6af2df8
Prateek Shah <pcshah1996@gmail.com>
2017-08-27 20:16:01 +0200

    Initial Commit

---
+++ TipsyReviews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipsyReviews
+//
+//  Created by Prateek Shah on 8/27/17.
+//  Copyright © 2017 Prateek Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
